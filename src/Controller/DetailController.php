a56525f7b62c5957c2e894e53ddef10c928bbae4
Jonas Vysocky <jonas.vysocky@gmail.com>
2022-09-29 00:15:23 +0200

    Prepare buttons on dashboard for any manipulation, Added delete functionality

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -7,6 +7,7 @@ use App\Service\FileUploader;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Form\CDFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,11 +17,16 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/cd/', name: 'cd_')]
 class DetailController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $em) {}
+    public function __construct(
+        private EntityManagerInterface $em,
+        private ManagerRegistry $doctrine
+    ) {}
 
     #[Route('detail/{id}', name: 'detail')]
-    public function detailCD(): Response
+    public function detailCD(Request $request, int $id): Response
     {
+        $cdDetail = $this->doctrine->getRepository(StoreCDs::class)->find($id);
+        dump($cdDetail);
         return $this->render('cd/detail.html.twig', [
             'controller_name' => 'DetailController',
         ]);
@@ -64,6 +70,12 @@ class DetailController extends AbstractController
     #[Route('detail/{id}/delete', name: 'delete')]
     public function deleteCD(int $id): RedirectResponse
     {
+        $cd = $this->doctrine->getManager()->getRepository(StoreCDs::class)
+            ->find($id)
+        ;
+        $this->em->remove($cd);
+        $this->em->flush();
+
         return $this->redirectToRoute('dashboard');
     }
 }
