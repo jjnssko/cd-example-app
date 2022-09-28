8f15403ac7be37f09368d2c7db2880a6e4fd63c7
Jonas Vysocky <jonas.vysocky@gmail.com>
2022-09-29 01:50:15 +0200

    Added pagination

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\StoreCDs;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,11 +15,23 @@ class DashboardController extends AbstractController
     public function __construct(private ManagerRegistry $doctrine) {}
 
     #[Route('/', name: 'dashboard')]
-    public function index(Request $request): Response
+    public function index(
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response
     {
+        $page = $request->query->getInt('page', 1);
+
         $cdsOverview = $this->doctrine->getRepository(StoreCDs::class)->findAll();
+        
+        $paginator = $paginator->paginate(
+            $cdsOverview,
+            $page,
+            5,
+            ['wrap-queries' => true],
+        );
         return $this->render('homepage/homepage.html.twig', [
-            'cds' => $cdsOverview,
+            'cds' => $paginator,
         ]);
     }
 }
