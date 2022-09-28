8f3e4f7eb618ab578fcd470f0789faedd1cc5d59
Jonas Vysocky <jonas.vysocky@gmail.com>
2022-09-28 22:01:20 +0200

    Added rough structure for page

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\StoreCDs;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function __construct(private ManagerRegistry $doctrine) {}
+
+    #[Route('/', name: 'dashboard')]
+    public function index(Request $request): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+        return $this->render('homepage/homepage.html.twig', [
         ]);
     }
 }
